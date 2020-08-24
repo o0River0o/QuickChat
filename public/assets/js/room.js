a65bb86bd310be8748e7d0e0a39bb417e2f83e2d
o0River0o <gyoushijie@gmail.com>
2020-08-24 13:47:17 -0700

    added more stuffs

--- public/assets/js/room.js
+++ public/assets/js/room.js
@@ -5,6 +5,9 @@ const peer = new Peer()
 
 const peers = {}
 
+var participants = 1;
+document.getElementById('users').innerText = participants;
+
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
@@ -23,16 +26,23 @@ navigator.mediaDevices.getUserMedia({
 
         call.on('stream', userVideoStream => {
             addVideoStream(video, userVideoStream);
+
         })
     })
 
     socket.on('user-connected', userId => {
         connectToNewUser(userId, stream);
+
+        participants++;
+        document.getElementById('users').innerText = participants;
     })
 })
 
 socket.on('user-disconnected', userId => {
-    if(peers[userId]) peers[userId].close();
+    if (peers[userId]) peers[userId].close();
+
+    participants--;
+    document.getElementById('users').innerText = participants;
 })
 
 peer.on('open', id => {
@@ -56,12 +66,40 @@ function connectToNewUser(userId, stream) {
 }
 
 function addVideoStream(video, stream) {
+
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
         video.play()
     })
 
     videoGrid.append(video);
+
 }
 
 //User Events
+
+function copyInvite() {
+    if (!window.getSelection) {
+        alert('Please copy the URL from the location bar.');
+        return;
+    }
+    const dummy = document.createElement('p');
+    dummy.textContent = window.location.href;
+    document.body.appendChild(dummy);
+
+    const range = document.createRange();
+    range.setStartBefore(dummy);
+    range.setEndAfter(dummy);
+
+    const selection = window.getSelection();
+    // First clear, in case the user already selected some other text
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
